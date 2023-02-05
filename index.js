8a068bc4386b7c56b4af4dd66ab99309270bfd30
hangu <gksrn42@gmail.com>
2023-02-05 16:53:05 +0900

    html create and vercel.json add

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 4000;
 
-app.use("/", (req, res) => {
-    res.send("Hello Express");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
 app.listen(PORT || 4000, () => {
     console.log("hello Server");
 });
+
+export default app;
